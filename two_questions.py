e2ec78c9934dcfc82fbd1737347ee39779d4035c
sea-yxli <126269311+sea-yxli@users.noreply.github.com>
2023-03-12 11:06:01 -0400

    Create two_questions.py

---
+++ two_questions.py
@@ -0,0 +1,17 @@
+category = int(input("Is it an animal, vegetable, or mineral?")).lower()
+bigger_than_breadbox = int(input("Is it bigger than a breadbox?")).lower()
+if category == "animal":
+  if bigger_than_breadbox == "yes":
+    print("My guess is that it is a bear.")
+  if bigger_than_breadbox == "no":
+    print("My guess is that it is a frog.")
+if category == "vegetable":
+  if bigger_than_breadbox == "yes":
+    print("My guess is that it is a pumpkin.")
+  if bigger_than_breadbox == "no":
+    print("My guess is that it is a tomato.")
+if category == "mineral":
+  if bigger_than_breadbox == "yes":
+    print("My guess is that it is a car.")
+  if bigger_than_breadbox == "no":
+    print("My guess is that it is a ring.") 
